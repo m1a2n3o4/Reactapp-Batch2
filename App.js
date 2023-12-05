979a1f1e531a379b80eaddcaf288a4ffebf19ec3
Manohar Lotlapalli <manoharlotlapalli@Manohars-MacBook-Air.local>
2023-12-05 20:02:27 +0530

    my new react App

---
+++ App.js
@@ -0,0 +1,33 @@
+// Create HTML Tag from ReactJs code(HEADING).
+/// By using React DOM - I want create a root.
+// I want render the Element ot the Root
+
+const heading = React.createElement("h1", { id: "myh1Tagtest"}, "Hey React"); // Any HTML Element.
+
+const root2 = ReactDOM.createRoot(document.getElementById("root"));
+
+root2.render(heading);
+
+// Development(Local Env) - QA (Quality check) - PROD (Production)
+
+//Dev
+//qa.flipkart.com (Serve)
+//flipkart.com //PRod (Serve) - Deploye 
+
+// React JS - WE dontt copy/paste
+
+// MiniFication, Bubdleing, Compress, Optimization ?
+
+// Build ?
+
+// Web-Packages  ?
+
+// How to Start a Applications ?
+
+// We need to install a Packages into our Application ()
+
+// NPM (Node packge Manager) - Managinf the Packages
+
+// Node js - Install
+
+// Terminal or CMD
